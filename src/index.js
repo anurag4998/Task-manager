bccd95659e454187b10e0a5eeba734385de37bbc
Anurag <anuragagrawal4998@gmail.com>
2020-09-18 18:44:14 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var cors = require('cors')
+
 require("./db/mongoose");
 
 const userRouter = require("./routers/user");
@@ -6,6 +8,7 @@ const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors())
 
 app.use(express.json());
 app.use(userRouter);
